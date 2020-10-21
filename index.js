cfc21dae35a8a70dc354aeb7807626396d0fcacf
Vanessa Porras <vanessapcg@gmail.com>
2020-10-21 05:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ var funkyFunction = function hi() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = hi()
+var theFunk = hi(funkyFunction)
